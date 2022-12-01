995f001adaddc9fd4a4a9cdd81120396bc879609
Loneow78 <amiasa@student.chalmers.se>
2022-12-01 17:06:39 +0100

    icon added to App two fxml loader file added & created MenuController class

--- src/main/java/snaked/App.java
+++ src/main/java/snaked/App.java
@@ -3,20 +3,25 @@ package snaked;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import snaked.model.GameOptions;
 
 import java.io.IOException;
 
 public class App extends Application {
+    Button startButton = new Button();
     @Override
     public void start(Stage stage) throws IOException {
         System.out.println(
-                GameOptions.fromJSON("src/main/resources/snaked/config/initialSettings.json")
+                GameOptions.fromJSON("resources/snaked/config/initialSettings.json")
         );
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view/MainMenu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Snake Game");
+        Image icon = new Image("resources/snaked/Icons/snake.png"); // icon for the app
+        stage.getIcons().add(icon); // setting the icon
         stage.setScene(scene);
         stage.show();
     }
