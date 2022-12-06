3a27620eaf96e1dcf5129c7bcb735577bdcfc6b2
gusbomjod <gusbomjod@student.gu.se>
2022-12-06 20:52:06 +0100

    Add colors to button indicating what options have been picked #2

--- src/main/java/snaked/sceneController/SettingsMenuController.java
+++ src/main/java/snaked/sceneController/SettingsMenuController.java
@@ -9,16 +9,29 @@ import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import snaked.App;
 
-
-
-
 public class SettingsMenuController {
     @FXML
     ImageView volumeIcon = new ImageView();
     @FXML
-    Button volumeButton = new Button();
+    Button easyButton = new Button();
     @FXML
-    Button backButton = new Button();
+    Button mediumButton = new Button();
+    @FXML
+    Button hardButton = new Button();
+    @FXML
+    Button map1Button = new Button();
+    @FXML
+    Button map2Button = new Button();
+    @FXML
+    Button map3Button = new Button();
+    @FXML
+    Button snake1Button = new Button();
+    @FXML
+    Button snake2Button = new Button();
+    @FXML
+    Button snake3Button = new Button();
+
+
     Image muteVolume = new Image("snaked/Icons/muteVolume.png");
     Image fullVolume = new Image("snaked/Icons/fullVolume.png");
 
@@ -31,10 +44,69 @@ public class SettingsMenuController {
             volumeIcon.setImage((fullVolume));
         }
     }
+
     @FXML //button that shows the original scene form the main menu
     protected void backButtonClick(ActionEvent event) {
         Node node = (Node) event.getSource();
         Stage stage = (Stage) node.getScene().getWindow();
         stage.setScene(App.mainMenu);
     }
+
+
+    @FXML //changes color of buttons to indicate what options have been chosen.
+    protected void easyButtonClick(){
+        easyButton.setStyle("-fx-background-color: #443c3c");
+        mediumButton.setStyle("-fx-background-color: #000000");
+        hardButton.setStyle("-fx-background-color: #000000");
+    }
+    @FXML
+    protected void mediumButtonClick(){
+        easyButton.setStyle("-fx-background-color: #000000");
+        mediumButton.setStyle("-fx-background-color: #443c3c");
+        hardButton.setStyle("-fx-background-color: #000000");
+    }
+    @FXML
+    protected void hardButtonClick(){
+        easyButton.setStyle("-fx-background-color: #000000");
+        mediumButton.setStyle("-fx-background-color: #000000");
+        hardButton.setStyle("-fx-background-color: #443c3c");
+    }
+    @FXML
+    protected void map1ButtonClick(){
+        map1Button.setStyle("-fx-background-color: #443c3c");
+        map2Button.setStyle("-fx-background-color: #000000");
+        map3Button.setStyle("-fx-background-color: #000000");
+    }
+    @FXML
+    protected void map2ButtonClick(){
+        map1Button.setStyle("-fx-background-color: #000000");
+        map2Button.setStyle("-fx-background-color: #443c3c");
+        map3Button.setStyle("-fx-background-color: #000000");
+    }
+    @FXML
+    protected void map3ButtonClick(){
+        map1Button.setStyle("-fx-background-color: #000000");
+        map2Button.setStyle("-fx-background-color: #000000");
+        map3Button.setStyle("-fx-background-color: #443c3c");
+    }
+    @FXML
+    protected void snake1ButtonClick(){
+        snake1Button.setStyle("-fx-background-color: #443c3c");
+        snake2Button.setStyle("-fx-background-color: #000000");
+        snake3Button.setStyle("-fx-background-color: #000000");
+    }
+    @FXML
+    protected void snake2ButtonClick(){
+        snake1Button.setStyle("-fx-background-color: #000000");
+        snake2Button.setStyle("-fx-background-color: #443c3c");
+        snake3Button.setStyle("-fx-background-color: #000000");
+    }
+    @FXML
+    protected void snake3ButtonClick(){
+        snake1Button.setStyle("-fx-background-color: #000000");
+        snake2Button.setStyle("-fx-background-color: #000000");
+        snake3Button.setStyle("-fx-background-color: #443c3c");
+    }
+
+
 }
